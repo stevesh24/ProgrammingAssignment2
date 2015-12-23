f3b2d24a2dc7d73f5a0ca74730b3e834f97ce4ce
stevesh24 <ibshimo2@gmail.com>
2015-12-23 01:41:17 -0800

    Steve's submission Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+###########################################################################
 
-## Write a short comment describing this function
+## Overview
+## Returns the inverse (m) of an input matrix (x). To save computing, m is 
+## computed once with solve() and cached.
+## On subsequent calls the cached inverse (m) is returned without computing. 
+
+## Instructions
+## 1. Call makeCacheMatrix(x) first to create list of matrix functions (the 
+##    special matrix object).
+##        e.g., matrix_object <- makeCacheMatrix(input_matrix)
+## 2. Then call cacheSolve() 
+##        e.g., inv_matrix <- cacheSolve(matrix_object)
+
+###########################################################################
+
+## makeCacheMatrix(): takes the input matrix (x) and creates a matrix 
+## 'object'(list with set,get,setinv,getinv) to be used by cacheSolve
+##    set: sets x to the input y matrix. sets the inverse matrix (m) to NULL.
+##    get: gets x
+##    setinv: finds inverse matrix (m) and assigns m to the inverse    
+##    getinv: gets inverse matrix (m)    
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+    x <<- y
+    m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
+###########################################################################
 
-## Write a short comment describing this function
+## cacheSolve() 
+## returns the inverse of x. The input is the list (special matrix
+## object) created by makeCacheMatrix(). 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    ## first use the function getinv to put cached inverse into m, 
+    ## and return m if it exists
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached inverse")
+        return(m)
+    }
+
+    ## Otherwise get the input matrix (x in makeCacheMatrix) with x$get, 
+    ## use solve to find the inverse of the data matrix, and set inverse
+    ## to m with the function setinv, and return m         
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
